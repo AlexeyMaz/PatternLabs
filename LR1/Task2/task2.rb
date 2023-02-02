8bcc7956f4879148e3a0c7b91d851b8f507093fd
AlMaz <alexmaznitskiy@gmail.com>
2023-02-03 00:35:09 +0300

    LR1_2.2 done

--- LR1/Task2/task2.rb
+++ LR1/Task2/task2.rb
@@ -12,6 +12,14 @@ def sum_np_divisors(x)
   sum
 end
 
+
+def digits_less_than3_cnt(x)
+  cnt = 0
+  x.digits.each { |digit| cnt += 1 if digit < 3}
+  cnt
+end
+
 puts 'Введите число: '
 num = gets.to_i
 puts "Сумма непростых делителей: #{sum_np_divisors(num)}"
+puts "Количество цифр, меньше 3: #{digits_less_than3_cnt(num)}"
