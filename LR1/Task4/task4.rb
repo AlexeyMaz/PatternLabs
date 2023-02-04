a9f6e54c807ee4c9a65e5c28c7eff98ed391aa24
AlMaz <alexmaznitskiy@gmail.com>
2023-02-04 06:37:15 +0300

    LR1_4_42 done

--- LR1/Task4/task4.rb
+++ LR1/Task4/task4.rb
@@ -5,16 +5,22 @@ def cycle_triple_shift_left(arr)
   arr.rotate(3)
 end
 
-
 # элементы перед первым минимальным
 def elems_b4_1st_min(arr)
   arr_min = arr.min
   arr[0, arr.index(arr_min)]
 end
 
+# локальный максимум
 def local_max?(arr, idx)
   idx = Integer(idx)
   return false unless idx.between?(1, arr.length - 2)
 
   arr[idx - 1] <= arr[idx] and arr[idx + 1] <= arr[idx]
+end
+
+# элементы, меньшие среднего арифметического
+def elems_less_than_avg(arr)
+  avg = arr.sum / arr.length.to_f
+  arr.filter { |x| x < avg }
 end
