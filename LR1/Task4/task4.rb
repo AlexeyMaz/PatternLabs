02849624f98f54466e6087901a3632f9a60665de
Sultan <alexmaznitskiy@gmail.com>
2023-02-04 09:09:48 +0300

    LR1_4 done

--- LR1/Task4/task4.rb
+++ LR1/Task4/task4.rb
@@ -29,3 +29,33 @@ end
 def elems_more_than_3_times(arr)
   arr.filter { |x| arr.count(x) > 3 }.uniq
 end
+
+file_path = 'C:/Users/sulta/OneDrive/Документы/GitHub/PatternLabs/LR1/Task4/array.txt'
+file = File.open(file_path)
+arr = file.readline.split(' ').map(&:to_i)
+
+puts "Введённый массив: #{arr.inspect}"
+puts "Какую задачу решать?
+1 - Циклический сдвиг влево на три позиции
+2 - Нахождение элементов перед первым минимальным
+3 - Нахождение локального максимума
+4 - Нахождение элементов меньше среднего арифметического
+5 - Нахождение элементов, встречающихся более трех раз"
+
+n = gets.chomp.to_i
+case n
+when 1
+  puts "Результат: #{cycle_triple_shift_left(arr)}"
+when 2
+  puts "Результат: #{elems_b4_1st_min(arr)}"
+when 3
+  puts 'Введите индекс предполагаемого локального максимума'
+  idx = gets.chomp.to_i
+  puts "Результат: #{local_max?(arr, idx)}"
+when 4
+  puts "Результат: #{elems_less_than_avg(arr)}"
+when 5
+  puts "Результат: #{elems_more_than_3_times(arr)}"
+else
+  puts 'Такого метода нет'
+end
