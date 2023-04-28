ba3335932579b1c3a59026fe1de2456b881ddb53
Al Maz <alexmaznitskiy@gmail.com>
2023-04-28 16:33:10 +0300

    teleg & telephone regex

--- LR2/Student.rb
+++ LR2/Student.rb
@@ -2,17 +2,18 @@ class Student
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
   PHONE_REGEX = /\A(\+7|8)\d{10}\z/
-
+  TELEGRAM_REGEX = /^@[A-Za-z0-9\-_]+$/
+  EMAIL_REGEX = /^[A-Za-z0-9\-_]+@[A-Za-z]+\.([A-Za-z]+\.)*[A-Za-z]+$/
 
   def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-    @id = id
-    @surname = surname
-    @name = name
-    @patronymic = patronymic
+    self.id = id
+    self.surname = surname
+    self.name = name
+    self.patronymic = patronymic
     self.phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+    self.telegram = telegram
+    self.email = email
+    self.git = git
   end
 
   def phone=(value)
@@ -20,6 +21,20 @@ class Student
       raise ArgumentError.new("Неправильный формат номера телефона: #{value}")
     end
     @phone = value
+  end
+
+  def telegram=(value)
+    if value && !value.match(TELEGRAM_REGEX)
+      raise ArgumentError.new("Неправильный формат телеграма: #{value}")
+    end
+    @telegram = value
+  end
+
+  def email=(value)
+    if value && !value.match(EMAIL_REGEX)
+      raise ArgumentError.new("Неправильный формат эл. почты: #{value}")
+    end
+    @email = value
   end
 
   def to_s
--- LR2/main.rb
+++ LR2/main.rb
@@ -1,4 +1,4 @@
 require_relative 'Student'
 
-stud1 = Student.new(surname: "Уайт", name: "Уолтер", patronymic: "Хартвелл", email: "dfs@gmail.", phone: '89001231231')
+stud1 = Student.new(surname: "dУайdт", name: "Уолтер", patronymic: "Хартвелл", email: "dfs@gmail.com", phone: '89001231231')
 print stud1.to_s
