6bfb59dca502f703e2968f4ce0ddda7fc1554378
Al Maz <alexmaznitskiy@gmail.com>
2023-04-28 15:42:59 +0300

    phone regex

--- LR2/Student.rb
+++ LR2/Student.rb
@@ -1,14 +1,25 @@
 class Student
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+
+  PHONE_REGEX = /\A(\+7|8)\d{10}\z/
+
+
   def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
     @id = id
     @surname = surname
     @name = name
     @patronymic = patronymic
-    @phone = phone
+    self.phone = phone
     @telegram = telegram
     @email = email
     @git = git
+  end
+
+  def phone=(value)
+    if value && !value.match(PHONE_REGEX)
+      raise ArgumentError.new("Неправильный формат номера телефона: #{value}")
+    end
+    @phone = value
   end
 
   def to_s
--- LR2/main.rb
+++ LR2/main.rb
@@ -1,10 +1,4 @@
 require_relative 'Student'
 
-stud1 = Student.new(surname: "Уайт", name: "Уолтер", patronymic: "Хартвелл", email: "dfs@gmail.", phone: '+7918')
+stud1 = Student.new(surname: "Уайт", name: "Уолтер", patronymic: "Хартвелл", email: "dfs@gmail.", phone: '89001231231')
 print stud1.to_s
-
-stud2 = Student.new(surname: "Пинкман", name: "Джесси", patronymic: "Брюс")
-print stud2.to_s
-
-stud3 = Student.new(surname: "Мазницкий", name: "Алексей", patronymic: "Андреевич")
-print stud3.to_s
