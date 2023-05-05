39e571fe0b61e38eab70ce52613c1a37f8166fb5
Al Maz <alexmaznitskiy@gmail.com>
2023-05-05 21:22:07 +0300

    data_list methods

--- LR2/data_list.rb
+++ LR2/data_list.rb
@@ -1,9 +1,42 @@
-class Data_list
-  def initialize(list:)
-    self.list = list
+class DataList
+
+  private_class_method :new
+
+  def initialize(objects)
+    self.objects_list = objects
+    self.selected_objects = []
   end
 
-  def some_method
-    raise NotImplementedError
+  def select(number)
+    selected_objects.append(number)
   end
+
+  def clear_selected
+    self.selected_objects = []
+  end
+
+  def get_selected
+    return [] if selected_objects.empty?
+
+    selected_id_list = []
+    selected_objects.each do |num|
+      selected_id_list.append(objects_list[num].id)
+    end
+
+    selected_id_list
+  end
+
+  protected
+
+  def get_names;
+    raise NotImplementedError, 'Будет реализовано в наследнике'
+  end
+
+  def get_data;
+    raise NotImplementedError, 'Будет реализовано в наследнике'
+  end
+
+  private
+
+  attr_accessor :selected_objects, :objects_list
 end
